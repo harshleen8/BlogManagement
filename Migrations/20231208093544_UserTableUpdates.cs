dd74b62098b19a383efa97f9b2e94ac28e17a835
Sadhnani <harshleen.sadhnani.082@my.csun.edu>
2023-12-13 19:33:37 -0800

    Updated code

---
+++ Migrations/20231208093544_UserTableUpdates.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerBlogManagement.Migrations
+{
+    public partial class UserTableUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+        }
+    }
+}
